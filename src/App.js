5ad90519606241d1c2286bfc3629a835dc07777a
Quoc Truong <truong.dinh@hoc10.com>
2022-04-07 13:59:28 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import Phaser from 'phaser';
-import GridEngine from 'grid-engine';
-import BootScene from './game/scenes/BootScene';
-import MainMenuScene from './game/scenes/MainMenuScene';
-import GameOverScene from './game/scenes/GameOverScene';
-import GameScene from './game/scenes/GameScene';
-import { makeStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
-import { Backdrop, Fade, Modal, Typography } from '@material-ui/core';
-import dialogBorderBox from './game/assets/images/dialog_borderbox.png';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import Phaser from "phaser";
+import GridEngine from "grid-engine";
+import BootScene from "./game/scenes/BootScene";
+import MainMenuScene from "./game/scenes/MainMenuScene";
+import GameOverScene from "./game/scenes/GameOverScene";
+import GameScene from "./game/scenes/GameScene";
+import { makeStyles } from "@material-ui/core/styles";
+import classNames from "classnames";
+import { Backdrop, Fade, Modal, Typography } from "@material-ui/core";
+import dialogBorderBox from "./game/assets/images/dialog_borderbox.png";
 import GameMenu from "./game/GameMenu";
 import DialogBox from "./game/DialogBox";
 import HeroCoin from "./game/HeroCoin";
 import HeroHealth from "./game/HeroHealth";
-import './App.css';
+import "./App.css";
 import { calculateGameSize } from "./game/utils";
+import ReactAudioPlayer from "react-audio-player";
+import apple from "./game/assets/audio/apple.mp3";
 
 const { width, height, multiplier } = calculateGameSize();
 
 const useStyles = makeStyles((theme) => ({
   modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    overflow: 'auto',
+    overflow: "auto",
   },
   postContainer: {
-    maxWidth: '90%',
-    maxHeight: '90%',
+    maxWidth: "90%",
+    maxHeight: "90%",
   },
   gameContentWrapper: {
     width: `${width * multiplier}px`,
     height: `${height * multiplier}px`,
-    margin: 'auto',
+    margin: "auto",
     padding: 0,
-    overflow: 'hidden',
-    '& canvas': {
-      imageRendering: 'pixelated',
-      '-ms-interpolation-mode': 'nearest-neighbor',
-      boxShadow: '0px 0px 0px 3px rgba(0,0,0,0.75)',
+    overflow: "hidden",
+    "& canvas": {
+      imageRendering: "pixelated",
+      "-ms-interpolation-mode": "nearest-neighbor",
+      boxShadow: "0px 0px 0px 3px rgba(0,0,0,0.75)",
     },
   },
   pageWrapper: {
@@ -54,64 +56,80 @@ const useStyles = makeStyles((theme) => ({
   },
   loadingText: {
     fontFamily: '"Press Start 2P"',
-    marginTop: '30px',
-    marginLeft: '30px',
+    marginTop: "30px",
+    marginLeft: "30px",
   },
   preLoadDialogImage: {
     backgroundImage: `url("${dialogBorderBox}")`,
-    backgroundSize: '1px',
-    backgroundRepeat: 'no-repeat',
+    backgroundSize: "1px",
+    backgroundRepeat: "no-repeat",
   },
   gameWrapper: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   gameGif: {
-    width: '100%',
-    position: 'absolute',
-    imageRendering: 'pixelated',
+    width: "100%",
+    position: "absolute",
+    imageRendering: "pixelated",
     top: 0,
   },
 }));
 
 const dialogs = {
-  "npc_01": [{
-    "message": "Hello",
-  }, {
-    "message": "How are you?",
-  }],
-  "npc_02": [{
-    "message": "Hello there",
-  }],
-  "npc_03": [{
-    "message": "Hi",
-  }, {
-    "message": "Ok bye!",
-  }],
-  "npc_04": [{
-    "message": "Hey",
-  }],
-  "sword": [{
-    "message": "You got a sword",
-  }],
-  "push": [{
-    "message": "You can push boxes now",
-  }],
-  "sign_01": [{
-    "message": "You can read this!",
-  }],
-  "book_01": [{
-    "message": "Welcome to the game!",
-  }]
+  npc_01: [
+    {
+      message: "Hello",
+    },
+  ],
+  npc_02: [
+    {
+      message: "Hello there",
+    },
+  ],
+  npc_03: [
+    {
+      message: "Hi",
+    },
+  ],
+  npc_04: [
+    {
+      message: "Hey",
+    },
+  ],
+  sword: [
+    {
+      message:
+        "Newton phát hiện ra định luật hấp dẫn sau khi bị quả gì rơi vào đầu?",
+      answer: "apple",
+      audio: apple,
+    },
+  ],
+  push: [
+    {
+      message: "You can push boxes now",
+    },
+  ],
+  sign_01: [
+    {
+      message: "You can read this!",
+    },
+  ],
+  book_01: [
+    {
+      message: "Welcome to the game!",
+    },
+  ],
 };
 
 function App() {
   const classes = useStyles();
   const [messages, setMessages] = useState([]);
-  const [characterName, setCharacterName] = useState('');
+  const [characterName, setCharacterName] = useState("");
   const [gameMenuItems, setGameMenuItems] = useState([]);
-  const [gameMenuPosition, setGameMenuPosition] = useState('center');
+  const [gameMenuPosition, setGameMenuPosition] = useState("center");
   const [heroHealthStates, setHeroHealthStates] = useState([]);
-  const [heroCoins, setHeroCoins] = useState(null);
+  const [heroCoins, setHeroCoins] = useState(0);
+  const [urlAudio, setUrlAudio] = useState("");
 
   const handleMessageIsDone = useCallback(() => {
     const customEvent = new CustomEvent(`${characterName}-dialog-finished`, {
@@ -120,13 +138,13 @@ function App() {
     window.dispatchEvent(customEvent);
 
     setMessages([]);
-    setCharacterName('');
+    setCharacterName("");
   }, [characterName]);
 
   const handleMenuItemSelected = useCallback((selectedItem) => {
     setGameMenuItems([]);
 
-    const customEvent = new CustomEvent('menu-item-selected', {
+    const customEvent = new CustomEvent("menu-item-selected", {
       detail: {
         selectedItem,
       },
@@ -137,10 +155,10 @@ function App() {
   useEffect(() => {
     const game = new Phaser.Game({
       type: Phaser.AUTO,
-      title: 'some-game-title',
-      parent: 'game-content',
+      title: "some-game-title",
+      parent: "game-content",
       orientation: Phaser.Scale.LANDSCAPE,
-      localStorageName: 'some-game-title',
+      localStorageName: "some-game-title",
       width,
       height,
       autoRound: true,
@@ -149,25 +167,20 @@ function App() {
         autoCenter: Phaser.Scale.CENTER_BOTH,
         mode: Phaser.Scale.ENVELOP,
       },
-      scene: [
-        BootScene,
-        MainMenuScene,
-        GameScene,
-        GameOverScene,
-      ],
+      scene: [BootScene, MainMenuScene, GameScene, GameOverScene],
       physics: {
-        default: 'arcade',
+        default: "arcade",
       },
       plugins: {
         scene: [
           {
-            key: 'gridEngine',
+            key: "gridEngine",
             plugin: GridEngine,
-            mapping: 'gridEngine',
+            mapping: "gridEngine",
           },
         ],
       },
-      backgroundColor: '#000000',
+      backgroundColor: "#000000",
     });
 
     // window.phaserGame = game;
@@ -176,92 +189,95 @@ function App() {
   useEffect(() => {
     const dialogBoxEventListener = ({ detail }) => {
       // TODO fallback
+      setUrlAudio(dialogs[detail.characterName][0].audio);
       setCharacterName(detail.characterName);
-      setMessages(
-          dialogs[detail.characterName]
-      );
+      setMessages(dialogs[detail.characterName]);
     };
-    window.addEventListener('new-dialog', dialogBoxEventListener);
+    window.addEventListener("new-dialog", dialogBoxEventListener);
 
     const gameMenuEventListener = ({ detail }) => {
       setGameMenuItems(detail.menuItems);
       setGameMenuPosition(detail.menuPosition);
     };
-    window.addEventListener('menu-items', gameMenuEventListener);
+    window.addEventListener("menu-items", gameMenuEventListener);
 
     const heroHealthEventListener = ({ detail }) => {
       setHeroHealthStates(detail.healthStates);
     };
-    window.addEventListener('hero-health', heroHealthEventListener);
+    window.addEventListener("hero-health", heroHealthEventListener);
 
     const heroCoinEventListener = ({ detail }) => {
-      setHeroCoins(detail.heroCoins);
+      // setHeroCoins(detail.heroCoins);
     };
-    window.addEventListener('hero-coin', heroCoinEventListener);
+    window.addEventListener("hero-coin", heroCoinEventListener);
 
     return () => {
-      window.removeEventListener('new-dialog', dialogBoxEventListener);
-      window.removeEventListener('menu-items', gameMenuEventListener);
-      window.removeEventListener('hero-health', heroHealthEventListener);
-      window.removeEventListener('hero-coin', heroCoinEventListener);
+      window.removeEventListener("new-dialog", dialogBoxEventListener);
+      window.removeEventListener("menu-items", gameMenuEventListener);
+      window.removeEventListener("hero-health", heroHealthEventListener);
+      window.removeEventListener("hero-coin", heroCoinEventListener);
     };
   }, [setCharacterName, setMessages]);
 
   return (
-      <div>
-        <div className={classes.gameWrapper}>
-          <div
-              id="game-content"
-              className={classes.gameContentWrapper}
-          >
-            {/* this is where the game canvas will be rendered */}
-          </div>
-          {heroHealthStates.length > 0 && (
-              <HeroHealth
-                  gameSize={{
-                    width,
-                    height,
-                    multiplier,
-                  }}
-                  healthStates={heroHealthStates}
-              />
-          )}
-          {heroCoins !== null && (
-              <HeroCoin
-                  gameSize={{
-                    width,
-                    height,
-                    multiplier,
-                  }}
-                  heroCoins={heroCoins}
-              />
-          )}
-          {messages.length > 0 && (
-              <DialogBox
-                  onDone={handleMessageIsDone}
-                  characterName={characterName}
-                  messages={messages}
-                  gameSize={{
-                    width,
-                    height,
-                    multiplier,
-                  }}
-              />
-          )}
-          {gameMenuItems.length > 0 && (
-              <GameMenu
-                  items={gameMenuItems}
-                  gameSize={{
-                    width,
-                    height,
-                    multiplier,
-                  }}
-                  position={gameMenuPosition}
-                  onSelected={handleMenuItemSelected}
-              />
-          )}
+    <div>
+      <ReactAudioPlayer
+        src={urlAudio}
+        autoPlay
+        controls
+        style={{ display: "none" }}
+      />
+      <div className={classes.gameWrapper}>
+        <div id="game-content" className={classes.gameContentWrapper}>
+          {/* this is where the game canvas will be rendered */}
         </div>
+        {heroHealthStates.length > 0 && (
+          <HeroHealth
+            gameSize={{
+              width,
+              height,
+              multiplier,
+            }}
+            healthStates={heroHealthStates}
+          />
+        )}
+        {heroCoins !== null && (
+          <HeroCoin
+            gameSize={{
+              width,
+              height,
+              multiplier,
+            }}
+            heroCoins={heroCoins}
+          />
+        )}
+        {messages.length > 0 && (
+          <DialogBox
+            onDone={handleMessageIsDone}
+            characterName={characterName}
+            messages={messages}
+            gameSize={{
+              width,
+              height,
+              multiplier,
+            }}
+            setHeroCoins={setHeroCoins}
+          />
+        )}
+        {gameMenuItems.length > 0 && (
+          <GameMenu
+            items={gameMenuItems}
+            gameSize={{
+              width,
+              height,
+              multiplier,
+            }}
+            position={gameMenuPosition}
+            onSelected={handleMenuItemSelected}
+          />
+        )}
       </div>
+    </div>
   );
 }
 
--- src/game/DialogBox.js
+++ src/game/DialogBox.js
@@ -1,117 +1,152 @@
-import { useCallback, useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { useCallback, useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useForm } from "react-hook-form";
+import dungAudio from "./assets/audio/dung.mp3";
+import saiAudio from "./assets/audio/sai.wav";
 
 // Images
-import dialogBorderBox from './assets/images/dialog_borderbox.png';
+import dialogBorderBox from "./assets/images/dialog_borderbox.png";
 
 // Components
-import Message from './Message';
+import Message from "./Message";
+import ReactAudioPlayer from "react-audio-player";
 
 const useStyles = makeStyles((theme) => ({
-    dialogWindow: ({ width, height, multiplier }) => {
-        const messageBoxHeight = Math.ceil((height / 3.5) * multiplier);
-        return {
-            imageRendering: 'pixelated',
-            fontFamily: '"Press Start 2P"',
-            textTransform: 'uppercase',
-            backgroundColor: '#e2b27e',
-            border: 'solid',
-            borderImage: `url("${dialogBorderBox}") 6 / ${6 * multiplier}px ${6 * multiplier}px ${6 * multiplier}px ${6 * multiplier}px stretch`,
-            padding: `${8 * multiplier}px`,
-            position: 'absolute',
-            top: `${Math.ceil((height * multiplier) - (messageBoxHeight + messageBoxHeight * 0.1))}px`,
-            width: `${Math.ceil(width * 0.8 * multiplier)}px`,
-            left: '50%',
-            transform: 'translate(-50%, 0%)',
-            minHeight: `${messageBoxHeight}px`,
-        };
-    },
-    dialogTitle: ({ multiplier }) => ({
-        fontSize: `${8 * multiplier}px`,
-        marginBottom: `${6 * multiplier}px`,
-        fontWeight: 'bold',
-    }),
-    dialogFooter: ({ multiplier }) => ({
-        fontSize: `${8 * multiplier}px`,
-        cursor: 'pointer',
-        textAlign: 'end',
-        position: 'absolute',
-        right: `${6 * multiplier}px`,
-        bottom: `${6 * multiplier}px`,
-    }),
+  dialogWindow: ({ width, height, multiplier }) => {
+    const messageBoxHeight = Math.ceil((height / 3.5) * multiplier);
+    return {
+      imageRendering: "pixelated",
+      fontFamily: '"Press Start 2P"',
+      textTransform: "uppercase",
+      backgroundColor: "#e2b27e",
+      border: "solid",
+      borderImage: `url("${dialogBorderBox}") 6 / ${6 * multiplier}px ${
+        6 * multiplier
+      }px ${6 * multiplier}px ${6 * multiplier}px stretch`,
+      padding: `${8 * multiplier}px`,
+      position: "absolute",
+      top: `${Math.ceil(
+        height * multiplier - (messageBoxHeight + messageBoxHeight * 0.1)
+      )}px`,
+      width: `${Math.ceil(width * 0.8 * multiplier)}px`,
+      left: "50%",
+      transform: "translate(-50%, 0%)",
+      minHeight: `${messageBoxHeight}px`,
+    };
+  },
+  dialogTitle: ({ multiplier }) => ({
+    fontSize: `${8 * multiplier}px`,
+    marginBottom: `${6 * multiplier}px`,
+    fontWeight: "bold",
+  }),
+  dialogFooter: ({ multiplier }) => ({
+    fontSize: `${8 * multiplier}px`,
+    cursor: "pointer",
+    textAlign: "end",
+    position: "absolute",
+    right: `${6 * multiplier}px`,
+    bottom: `${6 * multiplier}px`,
+  }),
 }));
 
 const DialogBox = ({
-    messages,
-    characterName,
-    onDone,
-    gameSize,
+  messages,
+  characterName,
+  onDone,
+  gameSize,
+  setHeroCoins,
 }) => {
-    const {
-        width,
-        height,
-        multiplier,
-    } = gameSize;
+  const { register, handleSubmit } = useForm();
+  const { width, height, multiplier } = gameSize;
 
-    const [currentMessage, setCurrentMessage] = useState(0);
-    const [messageEnded, setMessageEnded] = useState(false);
-    const [forceShowFullMessage, setForceShowFullMessage] = useState(false);
-    const classes = useStyles({
-        width,
-        height,
-        multiplier,
-    });
+  const [currentMessage, setCurrentMessage] = useState(0);
+  const [messageEnded, setMessageEnded] = useState(false);
+  const [forceShowFullMessage, setForceShowFullMessage] = useState(false);
+  const [results, setResults] = useState("");
+  const [urlAudio, setUrlAudio] = useState("");
+  const classes = useStyles({
+    width,
+    height,
+    multiplier,
+  });
 
-    const handleClick = useCallback(() => {
-        if (messageEnded) {
-            setMessageEnded(false);
-            setForceShowFullMessage(false);
-            if (currentMessage < messages.length - 1) {
-                setCurrentMessage(currentMessage + 1);
-            } else {
-                setCurrentMessage(0);
-                onDone();
-            }
-        } else {
-            setMessageEnded(true);
-            setForceShowFullMessage(true);
-        }
-    }, [currentMessage, messageEnded, messages.length, onDone]);
+  const onSubmit = (data) => {
+    const input = data.textInput.toLowerCase();
+    if (input === messages[0].answer) {
+      setHeroCoins((pre) => pre + 10);
+      setUrlAudio(dungAudio);
+      setResults("Đúng rồi");
+    } else {
+      setUrlAudio(saiAudio);
+      setHeroCoins((pre) => pre - 5);
+      setResults("Sai rồi");
+    }
+  };
 
-    useEffect(() => {
-        const handleKeyPressed = (e) => {
-            if (['Enter', 'Space', 'Escape'].includes(e.code)) {
-                handleClick();
-            }
-        };
-        window.addEventListener('keydown', handleKeyPressed);
+  const handleClick = useCallback(() => {
+    if (messageEnded) {
+      setMessageEnded(false);
+      setForceShowFullMessage(false);
+      if (currentMessage < messages.length - 1) {
+        setCurrentMessage(currentMessage + 1);
+      } else {
+        setCurrentMessage(0);
+        onDone();
+      }
+    } else {
+      setMessageEnded(true);
+      setForceShowFullMessage(true);
+    }
+  }, [currentMessage, messageEnded, messages.length, onDone]);
 
-        return () => window.removeEventListener('keydown', handleKeyPressed);
-    }, [handleClick]);
+  useEffect(() => {
+    const handleKeyPressed = (e) => {
+      if (["Enter", "Space", "Escape"].includes(e.code)) {
+        handleClick();
+      }
+    };
+    window.addEventListener("keydown", handleKeyPressed);
 
-    return (
-        <div className={classes.dialogWindow}>
-            <div className={classes.dialogTitle}>
-                {characterName}
-            </div>
-            <Message
-                action={messages[currentMessage].action}
-                message={messages[currentMessage].message}
-                key={currentMessage}
-                multiplier={multiplier}
-                forceShowFullMessage={forceShowFullMessage}
-                onMessageEnded={() => {
-                    setMessageEnded(true);
-                }}
-            />
-            <div
-                onClick={handleClick}
-                className={classes.dialogFooter}
-            >
-                {(currentMessage === messages.length - 1 && messageEnded) ? 'Ok' : 'Next'}
-            </div>
-        </div>
-    );
+    return () => window.removeEventListener("keydown", handleKeyPressed);
+  }, [handleClick]);
+
+  return (
+    <div className={classes.dialogWindow}>
+      <ReactAudioPlayer
+        src={urlAudio}
+        autoPlay
+        controls
+        style={{ display: "none" }}
+      />
+      <div className={classes.dialogTitle}>{characterName}</div>
+      <Message
+        action={messages[currentMessage].action}
+        message={messages[currentMessage].message}
+        key={currentMessage}
+        multiplier={multiplier}
+        forceShowFullMessage={forceShowFullMessage}
+        onMessageEnded={() => {
+          setMessageEnded(true);
+        }}
+      />
+
+      {!results ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            {...register("textInput")}
+            placeholder="Nhập từ vừa nghe được"
+          />
+          <button type="submit">Kiểm tra</button>
+        </form>
+      ) : (
+        <div>{results}</div>
+      )}
+      <div onClick={handleClick} className={classes.dialogFooter}>
+        {currentMessage === messages.length - 1 && messageEnded ? "Ok" : "Next"}
+      </div>
+    </div>
+  );
 };
 
 export default DialogBox;
